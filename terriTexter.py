37962beacce65863baaa70864d640a54277c9b7f
Alvin <alvin.liang@mail.utoronto.ca>
2022-05-21 02:11:25 -0400

    images and texting complete

--- terriTexter.py
+++ terriTexter.py
@@ -1,13 +1,32 @@
 from twilio.rest import Client 
-import keys
- 
-client = Client(keys.account_sid, keys.auth_token) 
- 
-message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
-                              body='Here is your Daily Terri Pic!',
-                              media_url= 'https://demo.twilio.com/owl.png',      
-                              to='whatsapp:+16476066782'
-                          )  
- 
-print(message.sid)
+from bs4 import BeautifulSoup
+import requests 
+import keys, time
+client = Client(keys.account_sid, keys.auth_token)
+
+source = requests.get('https://photos.app.goo.gl/S5S3p71fJVeqH8hL7').text
+#generate the url_array, through web scrapping 
+soup = BeautifulSoup(source, 'lxml')
+images = soup.findAll('img')
+# print(images)
+url_array = []
+total_pics = 0
+
+for image in images:
+    # print(image['src'])
+    url_array.append(str(image['src']))
+url_array.pop(0)
+
+# each API call requires a 1Cent each
+# send out daily Terri Pic  
+while (True):
+    for url in url_array:
+        message = client.messages.create(from_='+19207813947',  
+                                    body='Here is your Daily Terri Pic!',
+                                    media_url= url,      
+                                    to='+16476066782'
+                                )
+        total_pics += 1
+        print("Pic Number: " + str(total_pics))
+        time.sleep(86400)
+        
