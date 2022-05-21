f72a63e426d244542e780a98d7acf3b0f99e7f1a
Alvin <alvin.liang@mail.utoronto.ca>
2022-05-21 15:59:46 -0400

    switched to Imgur API

--- terriTexter.py
+++ terriTexter.py
@@ -1,32 +1,34 @@
 from twilio.rest import Client 
-from bs4 import BeautifulSoup
-import requests 
+from imgurpython import ImgurClient
 import keys, time
 client = Client(keys.account_sid, keys.auth_token)
+iClient = ImgurClient(keys.client_id, keys.client_secret)
 
-source = requests.get('https://photos.app.goo.gl/S5S3p71fJVeqH8hL7').text
-#generate the url_array, through web scrapping 
-soup = BeautifulSoup(source, 'lxml')
-images = soup.findAll('img')
-# print(images)
+
+
 url_array = []
 total_pics = 0
+# grab all the images from the Terri Album 
+items = iClient.get_album_images('y2gQt7t')
+for item in items:
+    url_array.append(item.link)
+print(url_array)
 
-for image in images:
-    # print(image['src'])
-    url_array.append(str(image['src']))
-url_array.pop(0)
-
-# each API call requires a 1Cent each
 # send out daily Terri Pic  
 while (True):
     for url in url_array:
-        message = client.messages.create(from_='+19207813947',  
-                                    body='Here is your Daily Terri Pic!',
+        message = client.messages.create(from_=keys.twilio_number,  
+                                    body='Here is your Daily Terri Pic!' + " (" + "We've sent: " + str(total_pics) + ")",
                                     media_url= url,      
                                     to='+16476066782'
                                 )
         total_pics += 1
         print("Pic Number: " + str(total_pics))
         time.sleep(86400)
-        
+
+# message = client.messages.create(
+#     to="+16476066782", 
+#     from_="+19207813947",
+#     body="It's Spooky",
+#     media_url= url_array[0])
+# print(message.sid)
